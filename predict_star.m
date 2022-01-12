545a33335209381f230d71fcc073dd0a167f20ed
Johan Kiviniemi <devel@johan.kiviniemi.name>
2022-01-12 03:59:39 +0200

    Move sight_reduction_step, predict_star into sight_reduction.m

--- predict_star.m
+++
@@ -1,6 +0,0 @@
-# Predict the azimuth and altitude of a star given an observer location and the
-# star's GP.
-function [ azimuth, altitude ] = predict_star(location, star_GP)
-  [ azimuth, distance ] = azimuth_distance(location, star_GP);
-  altitude = 0.5 * pi - distance;
-endfunction
--- sight_reduction.m
+++ sight_reduction.m
@@ -65,3 +65,21 @@ function print_new_position(old_position, new_position)
   printf("Position: %s\n", vector_str(new_position));
   printf("  Bearing: %s, distance: %s\n", dm_str(azimuth), dm_str(distance));
 endfunction
+
+# position: A new position estimate
+function new_position = sight_reduction_step(position, star_GP, observed_alt)
+  [ predicted_az, predicted_alt ] = predict_star(position, star_GP);
+  alt_diff = observed_alt - predicted_alt;
+
+  # If the observed altitude is higher, we are closer to the star than we
+  # thought. In that case, move the position toward the star's GP.
+  rot = azimuth_rotation(position, predicted_az, alt_diff);
+  new_position = azimuth_rotate(rot, position);
+endfunction
+
+# Predict the azimuth and altitude of a star given an observer location and the
+# star's GP.
+function [ azimuth, altitude ] = predict_star(location, star_GP)
+  [ azimuth, distance ] = azimuth_distance(location, star_GP);
+  altitude = 0.5 * pi - distance;
+endfunction
