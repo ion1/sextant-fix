171a6795a7276ab783764b6336d261db02cd4716
Johan Kiviniemi <devel@johan.kiviniemi.name>
2022-01-12 03:50:22 +0200

    ensure_normalized: Format difference with %g, not %f

--- ensure_normalized.m
+++ ensure_normalized.m
@@ -1,7 +1,7 @@
 function normalized_v = ensure_normalized(v)
   len = norm(v);
   if (abs(len - 1) > 1e-12)
-    warning("Got a normal with length = 1 + %f: %s", len - 1, mat2str(v))
+    warning("Got a normal with length = 1 + %g: %s", len - 1, mat2str(v))
   endif
   normalized_v = v ./ len;
 endfunction
