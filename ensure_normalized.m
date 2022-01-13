6a34ba979fd20cb97ab3ac126b305e8922308915
Johan Kiviniemi <devel@johan.kiviniemi.name>
2022-01-13 04:40:58 +0200

    ensure_normalized: Support multiple vectors

--- ensure_normalized.m
+++ ensure_normalized.m
@@ -1,7 +1,9 @@
 function normalized_v = ensure_normalized(v)
-  len = norm(v);
+  len = norm(v, "cols");
   if (abs(len - 1) > 1e-12)
-    warning("Got a normal with length = 1 + %g: %s", len - 1, mat2str(v))
+    warning(
+      "Got a normal with length = 1 + %s: %s",
+      mat2str(len - 1), mat2str(v));
   endif
   normalized_v = v ./ len;
 endfunction
