1446a20a8c98b2c87b0f1695be00d9a6e77d1990
Johan Kiviniemi <devel@johan.kiviniemi.name>
2022-02-17 07:30:16 +0200

    print_new_position: Don't print bearing/distance given a zero old position

--- print_new_position.m
+++ print_new_position.m
@@ -1,7 +1,9 @@
 function print_new_position(old_position, new_position, label = "Position:")
   printf("%s %s\n", label, vector_str(new_position));
-  printf(
-    "  Bearing: %s, distance: %s\n",
-    dm_str(azimuth(old_position, new_position)),
-    nautical_miles_str(distance(old_position, new_position)));
+  if (max(abs(old_position)) > 1e-6)
+    printf(
+      "  Bearing: %s, distance: %s\n",
+      dm_str(azimuth(old_position, new_position)),
+      nautical_miles_str(distance(old_position, new_position)));
+  endif
 endfunction
